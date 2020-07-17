da715cb921586740e021ceb1c4bf7a7a4c39b172
root <padialls@hotmail.com>
2020-07-17 06:24:54 -0300

    first commit/ bases de datos y crud especialidades-horaatencion-patologias-medicos

--- bin/first_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose exec app composer install
-docker-compose exec app chmod 777 ./web/assets -R
-docker-compose exec app chmod 777 .runtime -R 
-docker-compose exec app chmod 777 ./ -R
-
--- bin/yii.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose exec APP yii $@
